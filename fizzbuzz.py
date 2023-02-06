7039fc3ca87d7ab9d119aef5c6e0040394f03e4d
aliendh <gmrfh46@naver.com>
2023-02-06 23:48:31 +0900

    feat: Create fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,9 @@
-# Get Integer from user
-user_num = int(input('Hello, there! Enter some positive integer: '))
+# Get integer from user
+user_num = int(input("Hello, there! Enter some positive integer: "))
 
-# Iteration
+# Print user_num for debugging
+print(user_num)
+
+#Iteration
 for i in range(1, user_num+1):
-   if i%15==0:   
-     print('fizzbuzz')
-   elif i%5==0:
-     print('buzz')
-   elif i%3==0:
-     print('fizz')
-   else:
-     print(f'{i}')
+    print(f'{i}')
